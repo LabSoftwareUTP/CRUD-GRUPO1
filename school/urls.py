2a01c4519c9eb88070d886568c41185fa2a24a23
jamesw <jamersonwalderson@gmail.com>
2022-07-25 13:15:43 -0300

    update: progresso na correção das urls

--- school/urls.py
+++ school/urls.py
@@ -1,17 +1,18 @@
+from school import views
 from django.urls import path
 
 
 jornadas_urls = [
-    path('school'),
-    path('$', 'views.listarJornadas'),
-    path('new', 'views.newJornada'),
-    path('edit/(?P<id_jornada>.*)', 'views.editJornada'),
-    path('delete/(?P<id_jornada>.*)', 'views.deleteJornada'),
+    # path('school'),
+    path('', views.listarJornadas),
+    path('new', views.newJornada),
+    path('edit/(?P<id_jornada>.*)', views.editJornada),
+    path('delete/(?P<id_jornada>.*)', views.deleteJornada),
 ]
 PAcademico_urls = [
-    path('school'),
-    path('', 'views.listarPAcademico'),
-    path('new', 'views.newPAcademico'),
-    path('edit/(?P<id_pAcademico>.*)', 'views.editPAcademico'),
-    path('delete/(?P<id_pAcademico>.*)', 'views.deletePAcademico'),
+    # path('school'),
+    path('', views.listarPAcademico),
+    path('new', views.newPAcademico),
+    path('edit/(?P<id_pAcademico>.*)', views.editPAcademico),
+    path('delete/(?P<id_pAcademico>.*)', views.deletePAcademico),
 ]
