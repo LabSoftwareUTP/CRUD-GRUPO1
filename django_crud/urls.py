22a2a17eea44360641c319bc80ae3547e2d5b161
jamesw <jamersonwalderson@gmail.com>
2022-07-25 13:12:44 -0300

    update: corrigindo erro nas views

--- django_crud/urls.py
+++ django_crud/urls.py
@@ -1,18 +1,19 @@
-from django.conf.urls import patterns, include, url
+
+from django.conf.urls import include, url
 from school.urls import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
-    url(r'^$', 'website.views.home', name='home'),
+    path('', 'website.views.home', name='home'),
     # url(r'^django_crud/', include('django_crud.foo.urls')),
-    url(r'^jornadas/', include(jornadas_urls)),
-    url(r'^programas/', include(PAcademico_urls)),
+    path('jornadas/', include(jornadas_urls)),
+    path('programas/', include(PAcademico_urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+    path('admin/', include(admin.site.urls)),
+]
--- school/urls.py
+++ school/urls.py
@@ -1,14 +1,17 @@
-from django.conf.urls import patterns  # , url
+from django.urls import path
 
-jornadas_urls = patterns('school',
-    (r'^$', 'views.listarJornadas'),
-    (r'^new', 'views.newJornada'),
-    (r'^edit/(?P<id_jornada>.*)', 'views.editJornada'),
-    (r'^delete/(?P<id_jornada>.*)', 'views.deleteJornada'),
-)
-PAcademico_urls = patterns('school',
-    (r'^$', 'views.listarPAcademico'),
-    (r'^new', 'views.newPAcademico'),
-    (r'^edit/(?P<id_pAcademico>.*)', 'views.editPAcademico'),
-    (r'^delete/(?P<id_pAcademico>.*)', 'views.deletePAcademico'),
-)
+
+jornadas_urls = [
+    path('school'),
+    path('$', 'views.listarJornadas'),
+    path('new', 'views.newJornada'),
+    path('edit/(?P<id_jornada>.*)', 'views.editJornada'),
+    path('delete/(?P<id_jornada>.*)', 'views.deleteJornada'),
+]
+PAcademico_urls = [
+    path('school'),
+    path('', 'views.listarPAcademico'),
+    path('new', 'views.newPAcademico'),
+    path('edit/(?P<id_pAcademico>.*)', 'views.editPAcademico'),
+    path('delete/(?P<id_pAcademico>.*)', 'views.deletePAcademico'),
+]
