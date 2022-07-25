4a65087394ea2748e0efe13a4ffeef1fd0b6327b
jamesw <jamersonwalderson@gmail.com>
2022-07-25 13:23:08 -0300

    update: corrigindo erro de importação nas urls

--- django_crud/urls.py
+++ django_crud/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import include, url
 from school.urls import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from website import views
 admin.autodiscover()
 
 urlpatterns = [
     # Examples:
-    path('', 'website.views.home', name='home'),
+    path('', views.home, name='home'),
     # url(r'^django_crud/', include('django_crud.foo.urls')),
     path('jornadas/', include(jornadas_urls)),
     path('programas/', include(PAcademico_urls)),
@@ -15,5 +16,5 @@ urlpatterns = [
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    path('admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
 ]
