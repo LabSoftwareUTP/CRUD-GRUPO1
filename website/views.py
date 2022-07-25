3679ec1594ae331dc5480e705797d9b644d429e5
jamesw <jamersonwalderson@gmail.com>
2022-07-25 13:23:33 -0300

    update: substituindo função render_to_response por render

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 
 def home(request):
-	return render_to_response("home.html")
+	return render(request, "home.html")
