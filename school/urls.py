76e13a7439a96e30ac6e040dd3a44ab334e8ff14
jamesw <jamersonwalderson@gmail.com>
2022-07-29 15:41:06 -0300

    fix: adicionando tipagem nas url

--- school/urls.py
+++ school/urls.py
@@ -4,15 +4,15 @@ from django.urls import path
 
 jornadas_urls = [
     # path('school'),
-    path('', views.listarJornadas),
-    path('new', views.newJornada),
-    path('edit/(?P<id_jornada>.*)', views.editJornada),
-    path('delete/(?P<id_jornada>.*)', views.deleteJornada),
+    path('/', views.listarJornadas),
+    path('new/', views.newJornada),
+    path('edit/<int:id_jornada>', views.editJornada),
+    path('delete/<int:id_jornada>', views.deleteJornada),
 ]
 PAcademico_urls = [
     # path('school'),
     path('', views.listarPAcademico),
     path('new', views.newPAcademico),
-    path('edit/(?P<id_pAcademico>.*)', views.editPAcademico),
-    path('delete/(?P<id_pAcademico>.*)', views.deletePAcademico),
+    path('edit/<int:id_pAcademico>', views.editPAcademico),
+    path('delete/<int:id_pAcademico>', views.deletePAcademico),
 ]
