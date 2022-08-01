8dd7d2a10d9b6f26728aab37016552748f199882
jamesw <jamersonwalderson@gmail.com>
2022-07-31 21:40:35 -0300

    fix: corrigindo url home

--- django_crud/urls.py
+++ django_crud/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls import include, url
+from django.urls import include, path
 from school.urls import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,13 +9,13 @@ admin.autodiscover()
 
 urlpatterns = [
     # Examples:
-    path('', views.home, name='home'),
+    path('', views.home, name="home"),
     # url(r'^django_crud/', include('django_crud.foo.urls')),
     path('jornadas/', include(jornadas_urls)),
     path('programas/', include(PAcademico_urls)),
-    # Uncomment the admin/doc line below to enable admin documentation:
+    # # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    # # Uncomment the next line to enable the admin:
     path('admin/', admin.site.urls),
 ]
