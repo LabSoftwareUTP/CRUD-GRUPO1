388a1236eb1fc91e3e755bc8ae22c8b8b72a8127
jamesw <jamersonwalderson@gmail.com>
2022-07-31 21:51:18 -0300

    feat: adicionando name nas rotas de jornadas

--- school/urls.py
+++ school/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 
 jornadas_urls = [
     # path('school'),
-    path('/', views.listarJornadas),
-    path('new/', views.newJornada),
-    path('edit/<int:id_jornada>', views.editJornada),
-    path('delete/<int:id_jornada>', views.deleteJornada),
+    path('', views.listarJornadas, name="list-journey"),
+    path('new/', views.newJornada, name="create-journey"),
+    path('edit/<int:id_jornada>', views.editJornada, name="edit-journey"),
+    path('delete/<int:id_jornada>', views.deleteJornada, name="delete-journey"),
 ]
 PAcademico_urls = [
     # path('school'),
